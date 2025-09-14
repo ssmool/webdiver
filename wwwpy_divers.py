9270ab3c33a0fc1679141e3710744ed54b78d323
smool <igorfb4@hotmail.com>
2025-09-14 16:07:52 -0300

    Delete wwwpy_divers.py

--- wwwpy_divers.py
+++
@@ -1,6 +0,0 @@
-from webdiver.web_diver import *
-
-www_diver_add_task('https://electronics.howstuffworks.com/tv.htm')
-www_add_filter('ul','list-none border-b border-primary mb-2 pb-2')
-www_add_filter('li','my-2')
-www_diver_start('db_six.sqlite')
