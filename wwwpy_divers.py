735edd65be957d7738f50e33b5a027298eb3a1ff
smool <igorfb4@hotmail.com>
2025-09-14 16:12:26 -0300

    commit Release 2.0 Alpha

---
+++ wwwpy_divers.py
@@ -0,0 +1,7 @@
+from webdiver.web_diver import *
+
+#www_diver_add_task('https://electronics.howstuffworks.com/tv.htm') - to add PIPE FIFO URI DIVER
+#www_diver_start('db_six.sqlite','_text') - to get text content by URI
+#www_diver_start('db_six.sqlite','_html') - to get html content by URI
+#wwww_add_filter('dom_element','element_class_attr') - to set the filter by TAG and CLASS NAME FILTER
+#www_diver_start('db_six.sqlite','_filter') - to get text content by URI with DOM CLASS FILTER
