81f09f553c1884a93899a0407c815a7d4b6e6bc2
smool <igorfb4@hotmail.com>
2025-09-15 13:15:31 -0300

    commit 2.0 Alpha Relesase
    Added Shedule set_task()

--- wwwpy_divers.py
+++ wwwpy_divers.py
@@ -1,4 +1,10 @@
 from webdiver.web_diver import *
 
-www_diver_add_task('https://electronics.howstuffworks.com/tv.htm') # - to add PIPE FIFO URI DIVER
-www_diver_start('db_six.sqlite','_text') # - to get text content by URI
+_uri = 'https://github.com/ssmool/webdiver'
+_db = 'webdiver_db.sqlite'
+_hour = '13'
+_minute = '13'
+www_diver_add_task(_uri) # - to add PIPE FIFO URI DIVER
+www_diver_start(_db,'_html') # - to get HTML content by URI
+sql_setup(_db) # - to get database setting content by FILE_NAME
+set_task(_uri, _hour, _minute, '_text') # - to get text content by URI check the HOURS
